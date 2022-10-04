f91916d614176c5a1b7e0fdc75b7b69d399b751d
Wink <wink.code@gmail.com>
2022-10-04 12:40:45 +0530

    Adding all files to github in Aplle Products repo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes 
+} from "react-router-dom";
+
+import Home from './component/home/Home'
+import Navbar from './component/Navbar'
+import Shop from './component/Shop'
+import About from './component/About'
+import Blog from './component/Blog'
+import Contact from './component/Contact'
+import Pages from './component/Pages'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+    <>
+      
+     <Navbar />
+     <Routes>
+           <Route path='/' element={<Home />} />
+           <Route path= '/shop' element={<Shop />} />
+           <Route path= '/about' element={<About/>} />
+           <Route path= '/blog' element={<Blog />} />
+           <Route path= '/contact' element={<Contact />} />
+           <Route path= '/pages' element={<Pages />} />
+           
+     </Routes>
+     </>
+    </Router>
   );
 }
 
